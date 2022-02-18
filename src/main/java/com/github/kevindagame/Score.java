857477bf6ddb08241eb76f22a09f518591451e01
KevinDaGame <kevindolfing2@gmail.com>
2022-02-18 19:30:04 +0100

    Added current leaderboard placeholders

--- src/main/java/com/github/kevindagame/Score.java
+++ src/main/java/com/github/kevindagame/Score.java
@@ -3,9 +3,11 @@ package com.github.kevindagame;
 public class Score {
     private final String uuid;
     private int score;
-
-    public Score(String uuid) {
+    private String name;
+    public Score(String uuid, String name) {
         this.uuid = uuid;
+        this.name = name;
+
     }
 
     public int getScore() {
@@ -18,5 +20,9 @@ public class Score {
 
     public void addScore(int amount) {
         score += amount;
+    }
+
+    public Object getName() {
+        return name;
     }
 }
